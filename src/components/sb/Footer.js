0e8fe8c17ffa4045ad9e76fea3d9dd1173d5eb27
hassohamid <hassohamid@hotmail.com>
2025-09-03 04:31:05 +0200

    footer adjustm

--- src/components/sb/Footer.js
+++ src/components/sb/Footer.js
@@ -4,7 +4,10 @@ import FooterColumn from "./FooterColumn";
 
 export default function Footer({ blok }) {
   return (
-    <footer {...storyblokEditable(blok)} className="p-6 bg-gray-100 mt-12">
+    <footer
+      {...storyblokEditable(blok)}
+      className="p-6 bg-gray-100 fixed bottom-0 w-full"
+    >
       <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-4 max-w-7xl mx-auto">
         {blok.columns?.map((col) => (
           <FooterColumn blok={col} key={col._uid} />
