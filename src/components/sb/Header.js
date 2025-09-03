68f09ddea756c38ddb6aee50cecc46a61bdbd684
hassohamid <hassohamid@hotmail.com>
2025-09-03 03:33:02 +0200

    shadcn init

--- src/components/sb/Header.js
+++ src/components/sb/Header.js
@@ -2,6 +2,7 @@
 import { storyblokEditable } from "@storyblok/react";
 import MenuItem from "./MenuItem";
 import PromoBanner from "./PromoBanner";
+import { Search, ShoppingBag } from "lucide-react";
 
 export default function Header({ blok }) {
   return (
@@ -26,16 +27,27 @@ export default function Header({ blok }) {
               <MenuItem blok={item} key={item._uid} />
             ))}
           </nav>
+
+          {/* Search */}
+          {blok.search_enabled && (
+            <div className="relative ml-6">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <Search className="h-4 w-4 text-gray-400" />
+              </div>
+              <input
+                type="text"
+                placeholder="Search"
+                className="border border-gray-200 bg-white pl-10 rounded-md py-1.5 text-sm text-gray-500"
+              />
+            </div>
+          )}
         </div>
 
-        {/* Search */}
-        {blok.search_enabled && (
-          <input
-            type="text"
-            placeholder="Search..."
-            className="border rounded px-2 py-1"
-          />
-        )}
+        {/* Right side: Shopping Bag  - no actual func */}
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-5 w-5" />
+          <span className="text-sm">3</span>
+        </div>
       </div>
     </header>
   );
