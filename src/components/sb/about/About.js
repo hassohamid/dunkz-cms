47840b5b055969913edfc64d67e39b6a3ec52c5e
hassohamid <hassohamid@hotmail.com>
2025-09-05 05:42:33 +0200

    .

--- src/components/sb/about/About.js
+++ src/components/sb/about/About.js
@@ -7,7 +7,7 @@ export default function About({ blok }) {
   return (
     <div {...storyblokEditable(blok)}>
       {/* Hero Section with Title & Description */}
-      <section className="py-20 bg-white ">
+      <section className="py-20 bg-white  ">
         <div className=" text-center">
           {blok.title && (
             <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-8 tracking-tight">
@@ -15,22 +15,22 @@ export default function About({ blok }) {
             </h1>
           )}
           {blok.description && (
-            <p className="text-lg max-w-5xl mx-auto text-muted-foreground tracking-tighter leading-relaxed text-balance">
+            <p className="text-lg max-w-2xl mx-auto text-muted-foreground tracking-tighter leading-relaxed text-balance">
               {blok.description}
             </p>
           )}
 
           {blok.subheadline && (
-            <div className=" w-full mt-15">
-              <h1 className="text-4xl md:text-6xl font-thin tracking-widest   mb-8 mt-10  ">
+            <div className=" mt-15">
+              <h1 className="text-4xl md:text-5xl font-thin text-primary/40 border-t max-w-xs md:max-w-xl pt-5 mx-auto tracking-widest mb-8 mt-10  ">
                 {blok.subheadline}
               </h1>
             </div>
           )}
           {blok.subdescription && (
-            <div className="md:text-sm space-y-6 tracking-tighter text-center max-w-lg mx-auto text-xs border-t border-b py-3 text-balance px-15 md:px-0  ">
+            <div className="md:text-sm max-w-xs md:max-w-xl mx-auto text-left leading-relaxed space-y-5 tracking-tighter  text-xs border-b pb-10   ">
               {blok.subdescription.split("\n\n").map((paragraph) => (
-                <p className="  ">{paragraph}</p>
+                <p>{paragraph}</p>
               ))}
             </div>
           )}
