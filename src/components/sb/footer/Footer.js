a72bdc7b3905c6fef1c7e285e036fdfbb4b11cf1
hassohamid <hassohamid@hotmail.com>
2025-09-05 02:39:17 +0200

    added headline to reviews

--- src/components/sb/footer/Footer.js
+++ src/components/sb/footer/Footer.js
@@ -17,15 +17,33 @@ export default function Footer({ blok }) {
                 "Be the first to know about our special offers, new product launches, and events"}
             </p>
 
-            <div className="flex">
-              <input
-                type="email"
-                placeholder="Email Address"
-                className="flex-1 px-4 py-3 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
-              />
-              <button className="px-6 py-3 bg-gray-900 text-white font-medium rounded-r-md hover:bg-gray-800 transition-colors">
-                Sign Up
-              </button>
+            <div className="mt-6">
+              <div className="flex items-center gap-x-3">
+                <div className="relative">
+                  <svg
+                    className="w-6 h-6 text-gray-400 absolute left-3 inset-y-0 my-auto"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    className="w-full pl-12 pr-3 py-2 text-gray-500 bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                  />
+                </div>
+                <button className="block w-auto py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow">
+                  Subscribe
+                </button>
+              </div>
             </div>
           </div>
 
--- src/components/sb/reusable/Reviews.js
+++ src/components/sb/reusable/Reviews.js
@@ -2,17 +2,30 @@
 import { storyblokEditable } from "@storyblok/react";
 import { Marquee } from "@/components/magicui/marquee";
 import Review from "./Review";
+import { Heart } from "lucide-react";
 
 export default function Reviews({ blok }) {
   return (
-    <div {...storyblokEditable(blok)} className="py-20 relative ">
-      <Marquee pauseOnHover repeat={10}>
-        {blok.reviews?.map((review) => (
-          <Review key={review._uid} blok={review} />
-        ))}
-      </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+    <div {...storyblokEditable(blok)} className="py-20">
+      {blok.headline && (
+        <div className="w-full bg-white p-8 flex items-center justify-center mb-12 border-b border-l-green-400 border-l-7 max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto relative">
+          <h2 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-extralight tracking-[0.2em] text-center text-black antialiased uppercase">
+            {blok.headline}
+          </h2>
+
+          <Heart className="absolute top-2 right-8 w-4 h-4 md:w-6 md:h-6 text-lime-400 rotate-12 fill-current" />
+        </div>
+      )}
+      <div className="relative">
+        <Marquee pauseOnHover repeat={10}>
+          {blok.reviews?.map((review) => (
+            <Review key={review._uid} blok={review} />
+          ))}
+        </Marquee>
+
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+      </div>
     </div>
   );
 }
