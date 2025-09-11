b33a215cbe3129e85832dd82c3c2fe6e44ad24a3
hassohamid <hassohamid@hotmail.com>
2025-09-11 11:55:16 +0200

    menuItem diff design

--- src/components/sb/layout/header/MenuItem.js
+++ src/components/sb/layout/header/MenuItem.js
@@ -17,10 +17,31 @@ export default function MenuItem({ blok }) {
 
       {/* for the dropdown incase we create */}
       {blok.children?.length > 0 && (
-        <div className="absolute left-0 mt-2 hidden group-hover:block bg-white border rounded  p-2">
-          {blok.children.map((child) => (
-            <MenuItem blok={child} key={child._uid} />
-          ))}
+        <div
+          className="
+      absolute left-0 top-full z-50 pt-3
+      invisible opacity-0 -translate-y-1 pointer-events-none
+      transition-all duration-200 ease-out
+      group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto
+    "
+        >
+          <div
+            className="
+        min-w-[18rem] rounded-2xl border border-zinc-100 bg-white/90
+        backdrop-blur shadow-xl ring-1 ring-black/5
+      "
+          >
+            <div className="p-2 space-y-1">
+              {blok.children.map((child) => (
+                <div
+                  key={child._uid}
+                  className="rounded-xl px-3 py-2 hover:bg-zinc-50"
+                >
+                  <MenuItem blok={child} />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       )}
     </div>
